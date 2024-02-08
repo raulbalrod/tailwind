23ad085d7af0fa198b151c4a24688b4d10674b23
raulbalrod <119789823+raulbalrod@users.noreply.github.com>
2024-02-08 17:47:59 +0100

    Initial commit

---
+++ 01-get-started/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  './src/**/*.{html,js}',
+];
+
+export const theme = {
+  extend: {
+    colors: {
+      'primario': '#dedede',
+    }
+  }
+};
+
+export const plugins = [];
